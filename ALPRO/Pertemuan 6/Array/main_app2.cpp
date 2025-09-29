ef8aa5b9cc1eef2651abbc0a75be04a8e86cb5d8
Devin Rafa <devinrafa7@gmail.com>
2025-09-29 19:06:35 +0700

    pertemuan 6 alpro it's fixed

--- ALPRO/Pertemuan 6/Array/main_app2.cpp
+++ ALPRO/Pertemuan 6/Array/main_app2.cpp
@@ -1,21 +1,32 @@
+/*
+    Program Perkalian Matriks Dua Dimensi
+    I. S. = Program dimasukan nilai untuk variable array yang sudah ditentukan
+    F. S. = Program menghitung dan menentukan hasil dari perkalian matriks 2D
+*/
+
 #include <iostream>
 using namespace std;
 
 // Kamus Data
 int A[2][2] = {{1,2},{3,4}}; 
 int B[2][2] = {{5,6},{7,8}};
-int hasil[2][2];
+int AB[2][2];
 
+// Algoritma Utama
 int main(){
-    cout<<hasil[0][0]<<endl;
     for (int i=0; i<2; i++){
         for (int j=0; j<2; j++){
+            AB[i][j] = 0;
             for (int k=0; k<2; k++){
-                hasil[i][j] = hasil[i][j]+((A[j][k]*B[j][k])+(A[j][k]*B[j][k]));
-                
+                AB[i][j] = AB[i][j]+(A[i][k]*B[k][j]);
             }
-            cout<<hasil[i][j]<<" ";
         }
-        cout<<endl;
     }
+    for (int i=0; i<2; i++){
+        for (int j=0; j<2; j++){
+            cout<<AB[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
 }
