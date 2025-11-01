7ff7bb97e38086570b0fa8cff6a8cebdee12936a
DevinRafa <devinrafa7@gmail.com>
2025-11-01 08:47:22 +0700

    tugas bab 6

---
+++ Praktikum_ALPRO/Pertemuan 6 Array/5.1/main.c
@@ -0,0 +1,12 @@
+int ages[100];
+void main(){
+    // isi data ke array
+    for (i=0; i<100; i++){
+        ages[i]=i;
+    }
+
+    // cetak isi data
+    for (j=0; j<100; j++){
+        printf("&d\n", ages[j]);
+    }
+}
